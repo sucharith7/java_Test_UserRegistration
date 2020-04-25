acd50d75cb3cf34e4e9fc307766d97aa90cc510f
sucharith7 <ch.sucharith09@gmail.com>
2020-04-25 13:54:53 +0530

    [sucharith] added test firstname

---
+++ src/test/java/com/validateuser/ValidateUserTest.java
@@ -0,0 +1,15 @@
+package com.validateuser;
+
+import com.uservalidator.UserValidator;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ValidateUserTest {
+    @Test
+    public void givenFirstName_WhenMatched_ShouldReturnTrue() {
+        UserValidator userValidator = new UserValidator();
+        boolean result = userValidator.validateFirstName("Sucharith");
+        Assert.assertEquals(true,result);
+
+    }
+}
